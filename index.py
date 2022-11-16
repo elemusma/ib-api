7abfd58d8834a3408934bb078a552151322f471b
Efrain Lemus <efraintlemusm@gmail.com>
2022-11-15 22:04:34 -0500

    finish OOP Basics

--- index.py
+++ index.py
@@ -14,20 +14,39 @@
 # =============================================================================
 
 class employee:
-    def __inti__(self, name, emp_id, exp, dept): # this is a magic method, they will get executed automatically
-        # attributes of class employee
+    def __init__(self, name="Tim", emp_id="E0001", exp=5, dept="R&D"):
         self.Name = name
         self.Emp_id = emp_id
         self.Exp = exp
         self.Dept = dept
+        print("Employee {} is created".format(self.Emp_id))
         
     def calcSalary(self): #self is passing the object that gets created
         if self.Exp > 5 and self.Dept == "R&D":
             self.Salary = 200000
         else:
             self.Salary = 80000
+        print("Salary of {} calculated".format(self.Name))
             
     def empDesc(self):
-        print("Employee {} from {} department working with us for this many years" . format(self.Name,self.Dept,self.Exp))
-            
-        
+        print("Employee {} from {} department working with us for this many years".format(self.Name,self.Dept,self.Exp))
+
+# empl = employee()
+# print(empl.calcSalary())
+empl = employee(name="Dan",emp_id="E0002",exp=0,dept="HR")
+empl.calcSalary()
+print(empl.Salary)
+empl.empDesc()
+
+# class newEmployee:
+#     def __init__(self,name,salary):
+#         self.name = name
+#         self.salary = salary
+
+#     def get_salary(self):
+#         return self.salary
+
+# newEmp = newEmployee("Tadeo",600)
+
+# print(newEmp.name)
+# print(newEmp.get_salary())
