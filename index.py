ad13f5493c0f3ee451a51edb40ba64ebb55aa8fa
Efrain Lemus <efraintlemusm@gmail.com>
2022-11-15 22:20:51 -0500

    finish inheritance tutorial

--- index.py
+++ index.py
@@ -31,12 +31,11 @@ class employee:
     def empDesc(self):
         print("Employee {} from {} department working with us for this many years".format(self.Name,self.Dept,self.Exp))
 
-# empl = employee()
-# print(empl.calcSalary())
-empl = employee(name="Dan",emp_id="E0002",exp=0,dept="HR")
-empl.calcSalary()
-print(empl.Salary)
-empl.empDesc()
+
+# empl = employee(name="Dan",emp_id="E0002",exp=0,dept="HR")
+# empl.calcSalary()
+# print(empl.Salary)
+# empl.empDesc()
 
 # class newEmployee:
 #     def __init__(self,name,salary):
@@ -50,3 +49,25 @@ empl.empDesc()
 
 # print(newEmp.name)
 # print(newEmp.get_salary())
+
+# ================================
+# Inheritance: pass the properties of one class onto another
+# ================================
+
+class subEmployee(employee): #employee is the super class
+    def __init__(self, name="Tim", emp_id="E0001", exp=5, dept="R&D",sub_id="001"):
+        super(subEmployee,self).__init__(name,emp_id,exp,dept)
+        # self.Name = name
+        # self.Emp_id = emp_id
+        # self.Exp = exp
+        # self.Dept = dept
+        self.Sub_ID = sub_id
+        print("Employee {} is created for subsidiary {}".format(self.Emp_id, self.Sub_ID))
+
+    def calcSalary(self):
+        self.Salary = min(max(1,self.Exp)*30000,200000)
+
+empl_sub = subEmployee("Tadeo","E0004",10,"Marketing","7009")
+empl_sub.calcSalary()
+print(empl_sub.Salary)
+# print(empl_sub)
